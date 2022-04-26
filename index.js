8ac95a92f516c886cbfe48eb4668c56876eee105
Swikar Adhikari <np03a190013@heraldcollege.edu.np>
2022-04-26 12:21:41 +0545

    Removed Cors Origin

--- index.js
+++ index.js
@@ -8,16 +8,6 @@ const app = express();
 
 const PORT = 5000;
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
 app.use(bodyParser.json());
 
 app.use("/buyContent", buyContentRoute);
